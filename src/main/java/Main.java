ebcbeebe802056f064d28703487a3f0b460217f3
ahmed-farhanur-rashid <ahmed.farhanur.rashid@gmail.com>
2024-10-27 04:46:14 +0600

    Changed the code into equivalent lambda expression and method as IntelliJ won't stop bugging me.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,11 +2,7 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                new InputOutputUI(); // Create an instance of InputOutputUI
-            }
-        });
+
+        SwingUtilities.invokeLater(InputOutputUI::new);
     }
 }
