93836d5bfa43073dabd12d781fc55a75e47197f2
ahmed-farhanur-rashid <ahmed.farhanur.rashid@gmail.com>
2024-10-27 20:07:21 +0600

    Unnecessary, I did not even knew that was there.

--- src/main/java/ExpressionEvaluator.java
+++
@@ -1,9 +0,0 @@
-import net.objecthunter.exp4j.Expression;
-
-public class ExpressionEvaluator {
-
-    public static double f(Expression expression, double x) {
-        expression.setVariable("x", x);
-        return expression.evaluate();
-    }
-}
